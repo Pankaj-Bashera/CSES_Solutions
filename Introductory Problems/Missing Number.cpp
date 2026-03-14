91f1a2d31152eb250ddf92fdb27b37ddde63038d
Pankaj Bashera <146298510+Pankaj-Bashera@users.noreply.github.com>
2026-03-14 19:26:12 +0530

    Add Missing Number program in C++
    Implement a program to find the missing number in a sequence.

---
+++ Introductory Problems/Missing Number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+ 
+using namespace std;
+ 
+int main(){
+    long long n;
+    cin>>n;
+ 
+    vector<long long> arr(n-1);
+    long long sum = 0;
+ 
+    for(int i=0; i<n-1; i++) {
+        cin>>arr[i];
+        sum += arr[i];
+    }
+ 
+    cout<< (n*(n+1)/2) - sum << "\n";
+
+    return 0;
+}
